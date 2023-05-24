57878f4355de29ef87be0088b91448b5372258fd
IAMHUDA <hudamiftahul362@gmail.com>
2023-05-24 16:11:37 +0700

    memanggil fungsi main

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -59,7 +59,7 @@ void dokter::cetakPasien() {
 
 int main() {
 	dokter* varDokter1 = new dokter("dr.Budi");
-	dokter* varDokter2 = new dokter("dr.Tono");
+	dokter* varDokter2 = new dokter("dr.Tono"); 
 	pasien* varPasien1 = new pasien("Andi");
 	pasien* varPasien2 = new pasien("Lia");
 
