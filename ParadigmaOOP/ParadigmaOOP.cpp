c05db4431b31961cee3bb317fffd2cd8bb9db596
IAMHUDA <hudamiftahul362@gmail.com>
2023-05-24 15:29:55 +0700

    membuat fungsi int main

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -35,7 +35,7 @@ public:
 	}
 };
 
-int main() {
+int main() { 
 	pelajar siswa1("andi laksono", "SMAN 1 Bantul");
 	cout << siswa1.perkenalan();
 	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
